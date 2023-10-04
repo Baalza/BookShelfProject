a78ce49746f2b1f3d5f8bb6389f524f9ec3ad635
Niccolò Balzarotti <niccolobalzarotti@gmail.com>
2023-10-04 23:04:31 +0200

    implement angular

--- BookShelf/src/app/app.module.ts
+++ BookShelf/src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './mat-components/toolbar/toolbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FileManagerComponent } from './file-manager/file-manager.component';
+import { NavbarComponent } from './bootstrap-component/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FileManagerComponent,
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToolbarComponent,
+    FontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
