f2e1b441a3e8e87706b1001e0407e0d2f449ff5c
Niccolò Balzarotti <niccolobalzarotti@gmail.com>
2023-10-22 20:30:12 +0200

    implementing update user

--- BookShelf/src/app/bootstrap-component/canvas-component/canvas-component.component.ts
+++ BookShelf/src/app/bootstrap-component/canvas-component/canvas-component.component.ts
@@ -2,6 +2,8 @@ import {ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angu
 import {NgbOffcanvas, NgbOffcanvasOptions} from "@ng-bootstrap/ng-bootstrap";
 import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute, Router} from "@angular/router";
+import {FormControl, FormGroup} from "@angular/forms";
+import {values} from "ag-grid-community/dist/lib/utils/generic";
 
 
 @Component({
@@ -12,11 +14,15 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class CanvasComponentComponent implements OnInit {
   username: String = "";
   company: String = "";
+  userForm!: FormGroup;
+  name! : String;
+  comp! : String;
 
-  constructor(private canvasService: NgbOffcanvas,private route: ActivatedRoute) {
+  constructor(private canvasService: NgbOffcanvas,private route: ActivatedRoute,private http:HttpClient) {
   }
   @ViewChild('canvasElement', { static: true }) canvasElement!: ElementRef;
   ngOnInit(): void {
+    this.initForm();
     this.route.queryParams.subscribe(params => {
 
       this.username = params['username']
@@ -32,11 +38,31 @@ export class CanvasComponentComponent implements OnInit {
       window.location.reload();
     }
   }
+  initForm(): void {
+    this.userForm = new FormGroup({
+      //name: new FormControl(),
+      name: new FormControl({ value: this.username, disabled: true }),
+      company: new FormControl(this.company),
+    });
+  }
+  public onSubmit() {
+    const name = this.userForm.get('name')?.value;
+    const company = this.userForm.get('company')?.value;
+
+    console.log(name + " " + company);
+    const body = new FormData();
+    const options = { headers:{} };
+    body.append("username", name);
+    body.append("company", company);
+    /*this.http.post('http://localhost:8080/demo/registration', body, options)
+      .pipe(
+      ).subscribe((response: any) => {
+    });*/
+    }
   public set(username: any, company: any) {
     console.log(username, company)
     this.username = username;
     this.company = company;
-
   }
 
   public close(): void {
